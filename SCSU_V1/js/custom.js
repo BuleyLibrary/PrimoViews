031376514edfb8f58ade9924bd22a71835d37621
Parker Fruehan <fruehanp1@southernct.edu>
2021-08-31 15:04:17 -0400

    Remove Updates Bar and cleanup file structure for validation in Primo Studio.

--- SCSU_V1/js/custom.js
+++ SCSU_V1/js/custom.js
@@ -1,17 +1,10 @@
-(function () {
-    "use strict";
-    'use strict';
+(function(){
+"use strict";
+'use strict';
 
-
-    var app = angular.module('viewCustom', ['angularLoad']);
-
-    /****************************************************************************************************/
-
-        /*In case of CENTRAL_PACKAGE - comment out the below line to replace the other module definition*/
-
-        /*var app = angular.module('centralCustom', ['angularLoad']);*/
-
-    /****************************************************************************************************/
+var app = angular.module('viewCustom', ['angularLoad']);
+"use strict";
+'use strict';
 // Adds the chat button
 (function () {
     var lc = document.createElement('script');lc.type = 'text/javascript';lc.async = 'true';
@@ -20,15 +13,13 @@
 })();
 // End the chat button
 // Journal Button fixes
-app.controller('prmOpenJournalInFullController', [function () { var vm = this; vm.$onInit = function () { var resourceType = vm.parentCtrl.result.pnx.display.type[0] || ''; if (resourceType === 'journal') { vm.parentCtrl.isDirectLink = function () { return false; }; } }; }]); app.component('prmOpenJournalInFull', { bindings: { parentCtrl: '<' }, controller: 'prmOpenJournalInFullController' }); app.component('prmSearchResultAvailabilityLineAfter', { bindings: { parentCtrl: '<' }, template: '<prm-open-journal-in-full parent-ctrl="$ctrl.parentCtrl"></prm-open-journal-in-full>' });
-
- /* Announcement - BANNER PHF 1/22/2021 */
- app.component('scsuBannerComponent',  {
-    template: `<div style="width:100%; margin: auto; text-align: center;">See the <a href="https://libguides.southernct.edu/updates">Updates Guide</a> for the status of library services.</div>`
-})
-app.component('prmTopBarBefore', {
-    bindings: {parentCtrl: `<`},
-    template: `<scsu-banner-component></scsu-banner-component>`
-})
-/* Announcement - BANNER */
+app.controller('prmOpenJournalInFullController', [function () {
+    var vm = this;vm.$onInit = function () {
+        var resourceType = vm.parentCtrl.result.pnx.display.type[0] || '';if (resourceType === 'journal') {
+            vm.parentCtrl.isDirectLink = function () {
+                return false;
+            };
+        }
+    };
+}]);app.component('prmOpenJournalInFull', { bindings: { parentCtrl: '<' }, controller: 'prmOpenJournalInFullController' });app.component('prmSearchResultAvailabilityLineAfter', { bindings: { parentCtrl: '<' }, template: '<prm-open-journal-in-full parent-ctrl="$ctrl.parentCtrl"></prm-open-journal-in-full>' });
 })();
